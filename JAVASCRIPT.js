62fc0fbcfc5d60b80c8540778a56eb9769147497
cNobroc <132369996+cNobroc@users.noreply.github.com>
2023-09-13 08:10:26 -0500

    Update JAVASCRIPT.js

--- JAVASCRIPT.js
+++ JAVASCRIPT.js
@@ -1,30 +1,43 @@
-// funcion random para los valores de los elementos de la matriz
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
-// funcion random para el size de la matriz 
+}
+
 function getRandomInt2(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
-    
-// pedimos la cantidad de filas y columnas  
-var filas= prompt("ingrese el numero de filas")
-var columnas= prompt("ingrese el numero de columnas ")
+var botonAleatorio = document.getElementById("boton_aleatorio");
+var Matriz = new Array();
 
-/*CREANDO MATRIZ*/
-var Matriz= new Array();
+botonAleatorio.addEventListener("click", function() {
+   
 
- for(let i = 0; i < filas; i++ ){
-    Matriz[i] = new Array();
-    for(let j= 0; j< columnas; j++){
-        Matriz[i][j] = prompt(`Ingrese el valor para Matriz[${i}][${j}]`);
-        	    
+    var filas = getRandomInt2(5, 15);
+    var columnas = getRandomInt2(5, 15);
+
+    for (let i = 0; i < filas; i++) {
+        Matriz[i] = new Array();
+        for (let j = 0; j < columnas; j++) {
+            Matriz[i][j] = getRandomInt(2);
+        }
     }
-}
-/* MOSTRAR MATRIZ */
-for(let elemento in Matriz){
-    console.log(Matriz[elemento])
-}
 
+    console.log(Matriz); // Imprime la matriz completa en la consola
+});
+
+var botonPersonalizado= document.getElementById("boton_personalizado");
+
+botonPersonalizado.addEventListener("click", function(){
+    var filas = prompt("ingrese el numero de filas")
+    var columnas = prompt("ingrese el numero de columnas ")
+    for(let i= 0; i< filas; i++){
+        Matriz[i]= new Array();
+        for(let j= 0; j< columnas;j++){
+            Matriz[i][j]= prompt(`Ingrese el valor para la posición [${i}][${j}]`)
+        }
+    }
+    
+    console.log(Matriz);
+
+})
