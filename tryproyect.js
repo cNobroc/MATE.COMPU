6a916b784365b86e3d07e437b122cf0af6b0c6ed
cNobroc <132369996+cNobroc@users.noreply.github.com>
2023-10-25 10:31:13 -0500

    Update tryproyect.js

--- tryproyect.js
+++ tryproyect.js
@@ -16,9 +16,8 @@ var botonReset = document.getElementById("boton_reset");
 
 botonAleatorio.addEventListener("click", function() {
     if (Matriz === null) {
-        var filas = getRandomInt2(5, 15);
-        var columnas = getRandomInt2(5, 15);
-        Matriz = generarMatrizAleatoria(filas, columnas);
+        let n = getRandomInt2(5, 15);
+        Matriz = generarMatrizAleatoria(n);
         console.log("Matriz Original:");
         var matrizOriginal = JSON.parse(JSON.stringify(Matriz));
         console.log(matrizOriginal); 
@@ -36,9 +35,8 @@ botonAleatorio.addEventListener("click", function() {
 
 botonPersonalizado.addEventListener("click", function() {
     if (Matriz === null) {
-        var filas = prompt("Ingrese el número de filas");
-        var columnas = prompt("Ingrese el número de columnas");
-        Matriz = generarMatrizPersonalizada(filas, columnas);
+        let n = prompt("Ingrese el número de tamaño de la matriz");
+        Matriz = generarMatrizPersonalizada(n);
         console.log("Matriz Original:");
         var matrizOriginal = JSON.parse(JSON.stringify(Matriz));
         console.log(matrizOriginal); 
@@ -54,27 +52,35 @@ botonPersonalizado.addEventListener("click", function() {
 });
 
 botonReset.addEventListener("click", function() {
-    Matriz = null;  // Reiniciar la matriz a nula
+    Matriz = null;  
+    console.clear();
     console.log("Matriz reiniciada");
+   
 });
 
-function generarMatrizAleatoria(filas, columnas) {
+function generarMatrizAleatoria(n) {
     var Matriz = new Array();
-    for (let i = 0; i < filas; i++) {
+    for (let i = 0; i < n; i++) {
         Matriz[i] = new Array();
-        for (let j = 0; j < columnas; j++) {
+        for (let j = 0; j < n; j++) {
             Matriz[i][j] = getRandomInt(2);
         }
     }
     return Matriz;
 }
-
-function generarMatrizPersonalizada(filas, columnas) {
+// asdasd
+function generarMatrizPersonalizada(n) {
     var Matriz = new Array();
-    for (let i = 0; i < filas; i++) {
+    for (let i = 0; i < n; i++) {
         Matriz[i] = new Array();
-        for (let j = 0; j < columnas; j++) {
-            Matriz[i][j] = prompt(`Ingrese el valor para la posición [${i}][${j}]`);
+        for (let j = 0; j < n; j++) {
+            let valor;
+            do 
+            {
+               valor = prompt(`Ingrese el valor para la posición [${i}][${j}]`)
+            }
+            while(valor !== '1' && valor !== '0');
+            Matriz[i][j] = parseInt(valor);
         }
     }
     return Matriz;
@@ -88,7 +94,7 @@ function AgregacionDiagonal() {
         for (let i = 0; i < filas; i++) {
             for (let j = 0; j < columnas; j++) {
                 if (i === j) {
-                    Matriz[i][j] = 1; // Establece la diagonal principal a 1
+                    Matriz[i][j] = 1; 
                 }
             }
         }
@@ -98,25 +104,87 @@ function AgregacionDiagonal() {
 }
 
 function encontrarMatrizCaminos(matriz) {
-    const n = matriz.length; // Número de nodos en el grafo
-
-    // Inicializa la matriz de caminos con la misma estructura que la matriz original.
-    const matrizCaminos = new Array(n);
+    const n = matriz.length; 
     for (let i = 0; i < n; i++) {
-        matrizCaminos[i] = new Array(n);
-        for (let j = 0; j < n; j++) {
-            matrizCaminos[i][j] = matriz[i][j];
-        }
-    }
+        let j = (i !== 0) ? 0 : 1; 
+        while (j < n) {
+            if (matriz[i][j] === 1 && i !== j) {
+                let cambio = false;
+                for (let k = 0; k < n; k++) {
+                    
+                    const prev_val = matriz[i][k];
+                    matriz[i][k] = Math.max(matriz[i][k], matriz[j][k]);
 
-    // Aplica el algoritmo de Warshall para encontrar los caminos mínimos.
-    for (let k = 0; k < n; k++) {
-        for (let i = 0; i < n; i++) {
-            for (let j = 0; j < n; j++) {
-                matrizCaminos[i][j] = matrizCaminos[i][j] || (matrizCaminos[i][k] && matrizCaminos[k][j]);
+                 
+                    if (matriz[i][k] !== prev_val && k === j) {
+                        cambio = true;
+                    }
+                }
+               
+                j = cambio ? 0 : j + 1;
+            } else {
+                j++;
             }
         }
     }
+    return matriz;
+
+    function ordenandomatriz(matriz){
+        function reordenarMatriz(matriz) {
+            const n = matriz.length;
+            
+
+            const sumasFilas = matriz.map(fila => fila.reduce((a, b) => a + b, 0));
+            
+            
+            const indicesYSumas = sumasFilas.map((suma, indice) => [indice, suma]);
+            indicesYSumas.sort((a, b) => b[1] - a[1]);
+            
+            
+            const indicesOrdenados = indicesYSumas.map(item => item[0]);
+            
+           
+            const matrizOrdenada = indicesOrdenados.map(idx => matriz[idx]);
+            
+            
+            const matrizFinal = matrizOrdenada.map(fila =>
+                indicesOrdenados.map(idx => fila[idx])
+            );
+        
+            
+            process.stdout.write(" ");
+            indicesOrdenados.forEach(idx => process.stdout.write(idx + " "));
+            console.log();
+        
+            matrizFinal.forEach((fila, i) => {
+                process.stdout.write(i + " ");
+                fila.forEach(elem => process.stdout.write(elem + " "));
+                console.log();
+            });
+        }
+        
+       
+    }
+
+    return matrizCaminos;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return matrizCaminos;
 }
