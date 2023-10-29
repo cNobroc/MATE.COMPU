ebc77e34c885f0af293fbcbe4c9b4f1661279e5a
cNobroc <132369996+cNobroc@users.noreply.github.com>
2023-10-29 17:38:01 -0500

    Update tryproyect.js

--- tryproyect.js
+++ tryproyect.js
@@ -1,35 +1,3 @@
-// funcion random para los valores de los elementos de la matriz
-/*
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-// funcion random para el size de la matriz 
-function getRandomInt2(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-// pedimos la cantidad de filas y columnas  
-var filas = prompt("ingrese el numero de filas")
-var columnas = prompt("ingrese el numero de columnas ")
-*/
-
-/*CREANDO MATRIZ
-var Matriz = new Array();
-
-for (let i = 0; i < filas; i++) {
-    Matriz[i] = new Array();
-    for (let j = 0; j < columnas; j++) {
-        Matriz[i][j] = prompt(`Ingrese el valor para Matriz[${i}][${j}]`);
-
-    }
-}
-// MOSTRAR MATRIZ 
-for (let elemento in Matriz) {
-    console.log(Matriz[elemento])
-}
-*/
 var Matriz = null;  // Inicialmente, la matriz es nula
 
 function getRandomInt(max) {
@@ -184,9 +152,8 @@ function reordenando() {
     }
 
     Matriz = matrizFinal;
+    return Matriz
 }
-
-
 
 
 
